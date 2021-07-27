fdfef15f9529e296f4ef5c9b4eeee04c307767b8
antiqueDino <dinogbaguidi@gmail.com>
2021-07-28 00:08:33 +0200

    create-factory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Tags;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'content'];
+
+    public function tags()
+    {
+        return $this->hasMany(Tags::class);
+    }
 }
--- database/migrations/2021_07_27_211908_create_posts_table.php
+++ database/migrations/2021_07_27_211908_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('titlte');
+            $table->string('title');
             $table->longText('content');
             $table->timestamps();
         });
